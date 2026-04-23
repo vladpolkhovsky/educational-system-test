36d4becd1e4e88ec2f10cad3404dd965bd98b380
vladpolkhovsky <vlad.polkhovsky@gmail.com>
2026-04-23 11:23:18 +0300

    23.04.2026

--- start.sh
+++ start.sh
@@ -1,15 +1,15 @@
-kubectl create secret tls local-selfsigned-tls --cert=tls.crt --key=tls.key --namespace traefik
-
 helm repo add traefik https://traefik.github.io/charts
 helm repo add collabora https://collaboraonline.github.io/online
-helm install traefik traefik/traefik --namespace traefik --values .\traefik\values.yaml
 
 kubectl create namespace traefik
-kubectl create secret tls local-selfsigned-tls --cert=.\traefik\tls.crt --key=.\traefik\tls.key --namespace traefik
 
-helm install traefik traefik/traefik --namespace traefik --values .\traefik\values.yaml
-kubectl apply -f .\traefik\configmap.yaml
-kubectl apply -f .\traefik\deployment.yaml
+# Create ACME secret for Let's Encrypt certificates storage
+kubectl create secret generic acme-json --from-file=acme.json=./traefik/acme.json --namespace traefik || true
 
-helm install --namespace traefik collabora-online collabora/collabora-online -f .\collabora\values.yaml
+helm install traefik traefik/traefik --namespace traefik --values ./traefik/values.yaml
+
+kubectl apply -f ./mirotalk-p2p/configmap.yaml
+kubectl apply -f ./mirotalk-p2p/deployment.yaml
+
+helm install --namespace traefik collabora-online collabora/collabora-online -f ./collabora/values.yaml
 kubectl delete hpa collabora-online -n traefik --ignore-not-found=true
